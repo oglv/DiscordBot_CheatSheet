83ee6c0f78138538ff86c8166ca5396f285ae81d
oglv <132039999+oglv@users.noreply.github.com>
2023-12-10 22:41:06 +0900

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -9,9 +9,9 @@ const commands = [
     new SlashCommandBuilder().setName('Write Command').setDescription('Command explanation'),
 ]
     .map(command => command.toJSON());
+
+
 const rest = new REST({ version: '9' }).setToken(token);
-
-
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
     .then(() => console.log('Successfully registered application commands.'))
     .catch(console.error);
