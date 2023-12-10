7d3de89adbc1120ec26840859845861c83b14811
oglv <132039999+oglv@users.noreply.github.com>
2023-12-10 22:30:28 +0900

    add explanation

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,11 +2,16 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
+
+//add command
+
 const commands = [
     new SlashCommandBuilder().setName('Write Command').setDescription('Command explanation'),
 ]
     .map(command => command.toJSON());
 const rest = new REST({ version: '9' }).setToken(token);
+
+
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
     .then(() => console.log('Successfully registered application commands.'))
     .catch(console.error);
